32350e9999a07c5159d4d3a618c9afd6139a55d7
roman <romka69tver@gmail.com>
2019-09-17 02:31:53 +0300

    add list of clients in dashboard (frontend)

--- app/javascript/staff/components/dashboard.vue
+++ app/javascript/staff/components/dashboard.vue
@@ -3,14 +3,25 @@
     h2 Create new client
     form
       .group
-        input(v-model="fullname" placeholder="Client full name")
-        input(v-model="phone" placeholder="Client phone")
-        input(v-model="email" placeholder="Client email" type="email")
+        input(v-model.trim="fullname" placeholder="Client full name")
+        input(v-model.trim="phone" placeholder="Client phone")
+        input(v-model.trim="email" placeholder="Client email" type="email")
       .action
         button(v-if="showFields" @click="sendForm") Submit
+    hr
+    section(class="clients")
+      h2 List of clients
+      table
+        tr
+          th Full name
+          th Number phone
+          th Email
+        client(v-for="client in clients" :client="client" :key="client.id")
 </template>
 
 <script>
+  import Client from './client.vue'
+
   let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,})$/
 
   export default {
@@ -18,7 +29,21 @@
       return {
         fullname: '',
         phone: '',
-        email: ''
+        email: '',
+        clients: [
+          {
+            id: 1,
+            fullname: 'Bob Derp',
+            phone: '123321',
+            email: 'bob@derp.com'
+          },
+          {
+            id: 2,
+            fullname: 'Foo Bar',
+            phone: '098890',
+            email: 'foo@bar.com'
+          }
+        ]
       }
     },
     computed: {
@@ -38,6 +63,9 @@
       },
       sendForm() {
       }
+    },
+    components: {
+      Client
     }
   }
 </script>
@@ -49,7 +77,7 @@
   input {
     display: block;
     padding: 10px;
-    font-size: 15px;
+    font-size: 18px;
     width: 250px;
     margin-left: auto;
     margin-right: auto;
@@ -75,6 +103,23 @@
     transition: all 0.4s ease 0s;
   }
   hr {
-    margin: 0;
+    margin: 10px 10px;
+  }
+  section.clients {
+    margin-right: 20%;
+    margin-left: 20%;
+  }
+  table {
+    width: 100%;
+  }
+  tr {
+    line-height: 40px;
+    text-align: center;
+  }
+  th {
+    font-size: 20px;
+  }
+  td {
+    font-size: 18px;
   }
 </style>
