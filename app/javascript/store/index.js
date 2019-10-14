c2151b577d29c37251b85cf6a66032569eae6606
roman <romka69tver@gmail.com>
2019-10-14 17:02:04 +0300

    add component filter with vuex-persisted

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    filter: ''
+  },
+  mutations: {
+    updateFilter(state, value) {
+      state.filter = value
+    }
+  },
   actions: {},
   modules: {},
-  plugins: []
+  plugins: [createPersistedState()]
 })
